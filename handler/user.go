b8d11488f8f3f04d9e397954b07730c88dd4f772
Fathur Rahman <fathur.rahman@udana.id>
2022-02-23 22:35:06 +0700

    [Add] Login handler

--- handler/user.go
+++ handler/user.go
@@ -57,4 +57,31 @@ func (h *userHandler) Login(c *gin.Context) {
 	// input struct passing service
 	// di service mencari dg bantuan repository user dengan email x
 	// mencocokan password
+
+	var input user.LoginInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Login failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	loggedinUser, err := h.userService.Login(input)
+
+	if err != nil {
+		errorMessage := gin.H{"errors": err.Error()}
+		response := helper.APIResponse("Login failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	formatter := user.FormatUser(loggedinUser, "tokentokentoken")
+
+	response := helper.APIResponse("Successfuly loggedin", http.StatusOK, "success", formatter)
+
+	c.JSON(http.StatusOK, response)
 }
