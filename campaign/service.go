2fa39bd2acbf35263152ea73a0eb1589a072d9d0
Fathur Rahman <fathur.rahman@udana.id>
2022-03-13 02:18:06 +0700

    Campaign Detail API

--- campaign/service.go
+++ campaign/service.go
@@ -1,7 +1,10 @@
 package campaign
 
+import "errors"
+
 type Service interface {
 	GetCampaigns(userID int) ([]Campaign, error)
+	GetCampaignID(input GetCampaignDetailInput) (Campaign, error)
 }
 
 type service struct {
@@ -28,4 +31,18 @@ func (s *service) GetCampaigns(userID int) ([]Campaign, error) {
 	}
 
 	return campaigns, nil
+}
+
+func (s *service) GetCampaignID(input GetCampaignDetailInput) (Campaign, error) {
+	campaign, err := s.repository.FindByID(input.ID)
+
+	if err != nil {
+		return campaign, err
+	}
+
+	if campaign.ID == 0 {
+		return campaign, errors.New("No user found on with that ID")
+	}
+
+	return campaign, nil
 }
