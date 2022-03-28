b3a6330106d00b1c08eb1bbb0aff17bedc996267
Fathur Rahman <fathur.rahman@udana.id>
2022-03-29 00:41:43 +0700

    create campaign submit

--- main.go
+++ main.go
@@ -166,6 +166,7 @@ func main() {
 
 	router.GET("/campaigns", campaignWebHandler.Index)
 	router.GET("/campaigns/new", campaignWebHandler.New)
+	router.POST("/campaigns",  campaignWebHandler.Create)
 
 	router.Run(":8080")
 
