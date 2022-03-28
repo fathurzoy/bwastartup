f81712248dec6f4c9d848980d36989c2b1079a1d
Fathur Rahman <fathur.rahman@udana.id>
2022-03-28 22:11:24 +0700

    user update

--- main.go
+++ main.go
@@ -158,6 +158,9 @@ func main() {
 	router.GET("/users", userWebHandler.Index)
 	router.GET("/users/new", userWebHandler.New)
 	router.POST("/users", userWebHandler.Create)
+	router.GET("/users/edit/:id", userWebHandler.Edit)
+	router.POST("/users/update/:id", userWebHandler.Update)
+
 
 	router.Run(":8080")
 
--- web/handler/user.go
+++ web/handler/user.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"bwastartup/user"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -46,6 +47,50 @@ func (h *userHandler) Create(c *gin.Context){
 
 	_, err = h.userService.RegisterUser(registerInput)
 	if err != nil{
+		c.HTML(http.StatusInternalServerError, "error.html", nil)
+		return
+	}
+
+	c.Redirect(http.StatusFound, "/users")
+}
+
+
+func (h *userHandler) Edit(c *gin.Context) {
+	idParam := c.Param("id")
+	id, _ := strconv.Atoi(idParam)
+
+	registeredUser, err := h.userService.GetUserByID(id)
+	if err != nil {
+		c.HTML(http.StatusInternalServerError, "error.html", nil)
+		return
+	}
+
+	input := user.FormUpdateUserInput{}
+	input.ID = registeredUser.ID
+	input.Name = registeredUser.Name
+	input.Email = registeredUser.Email
+	input.Occupation = registeredUser.Occupation
+
+	c.HTML(http.StatusOK, "user_edit.html", input)
+}
+
+func (h *userHandler) Update(c *gin.Context) {
+	idParam := c.Param("id")
+	id, _ := strconv.Atoi(idParam)
+
+	var input user.FormUpdateUserInput
+
+	err := c.ShouldBind(&input)
+	if err != nil {
+		input.Error = err
+		c.HTML(http.StatusOK, "user_edit.html", input)
+		return
+	}
+
+	input.ID = id
+
+	_, err = h.userService.UpdateUser(input)
+	if err != nil {
 		c.HTML(http.StatusInternalServerError, "error.html", nil)
 		return
 	}
